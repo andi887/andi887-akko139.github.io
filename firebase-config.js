ca105911dd811c2568c20b80ed936115a97e70a9
andi139 <akkoandi@gmail.com>
2025-12-20 12:39:52 +0800

    Update firebase-config.js
    Signed-off-by: andi139 <akkoandi@gmail.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,7 @@
 // firebase-config.js
-// Project: nomoromosse
-// Digunakan oleh semua fitur: fitur1.html, fitur2.html, ..., dasar.html
-
 const firebaseConfig = {
   apiKey: "AIzaSyALc-1SYjzRUcB-Ws01rab1mxlmcv2e6dA",
-  authDomain: "nomoromosse.firebaseapp
-.com",
+  authDomain: "nomoromosse.firebaseapp.com",
   databaseURL: "https://nomoromosse-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "nomoromosse",
   storageBucket: "nomoromosse.firebasestorage.app",
@@ -13,10 +9,7 @@ const firebaseConfig = {
   appId: "1:985183830784:web:fae21bdb94c1f0103769e5"
 };
 
-// Inisialisasi Firebase hanya sekali
-if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+// Inisialisasi hanya sekali
+if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
-// Ekspos database untuk penggunaan di fitur lain
-const database = firebase.database();
